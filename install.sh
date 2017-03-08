4072d1c47e6047b3413fbb0ff6cfd2f383c8fd42
Chris <electronicsleep@gmail.com>
2017-03-08 01:04:02 -0800

    Install Debian 8 Nvidia steps

--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
 ### INSTALL NVIDIA DRIVERS DEBIAN 8
 
+#DOWNLOAD Card/Arch
+#http://www.nvidia.com/Download/index.aspx?lang=en-us
+
 #apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get install build-essential && apt-get install linux-source apt-get install linux-headers-$(uname -r)
 
 #vim /etc/default/grub
 
 #GRUB_CMDLINE_LINUX_DEFAULT="quiet splash nomodeset"
 
-#Replace with
-
+#Replace
 #GRUB_CMDLINE_LINUX_DEFAULT="nouveau.blacklist=1 quiet splash nomodeset"
-
-#update-grub2
 
 #REMOVE ANY CONFLICTS
 #apt-get remove nvidia* && apt-get autoremove
 
-#REBOOT SYSTEM
 #reboot
 
 #vim /etc/modprobe.d/blacklist.conf
@@ -36,7 +35,8 @@
 #reboot
 
 #GRUB_CMDLINE_LINUX_DEFAULT="nouveau.blacklist=1 quiet splash nomodeset"
-#Replace it with;
+
+#Replace
 #GRUB_CMDLINE_LINUX_DEFAULT="quiet splash nomodeset"
 
 #INSTALL STEAM AND ENJOY
