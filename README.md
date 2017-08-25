451f5c8b31261d867d4afb62a50ce3e3d7903efa
Chris <electronicsleep@gmail.com>
2017-08-24 18:57:01 -0700

    Updated readability

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Nvidia
+# NvidiaDebian
+
+Purpose: Install Nvidia drivers Debian 8 
+
+Should run on any Linux disto, let me know if that is not the case
+
+Released under the BSD license
--- install.sh
+++ install.sh
@@ -1,41 +1,47 @@
-### INSTALL NVIDIA DRIVERS DEBIAN 8
-#DOWNLOAD Card/Arch
-#http://www.nvidia.com/Download/index.aspx?lang=en-us
+# Author: https://github.com/electronicsleep
+# Purpose: Install Nvidia drivers Debian 8 
+# Should run on any Linux disto, let me know if that is not the case
+# Released under the BSD license
 
-#apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get install build-essential && apt-get install linux-source apt-get install linux-headers-$(uname -r)
+# DOWNLOAD Card for Arch
+# http://www.nvidia.com/Download/index.aspx?lang=en-us
 
-#vim /etc/default/grub
-#GRUB_CMDLINE_LINUX_DEFAULT="quiet splash nomodeset"
-#Replace
-#GRUB_CMDLINE_LINUX_DEFAULT="nouveau.blacklist=1 quiet splash nomodeset"
+# apt-get update && apt-get upgrade && apt-get dist-upgrade
+# apt-get install build-essential linux-source linux-headers-$(uname -r)
 
-#REMOVE ANY CONFLICTS
-#apt-get remove nvidia* && apt-get autoremove
+# vim /etc/default/grub
+# Find
+# GRUB_CMDLINE_LINUX_DEFAULT="quiet splash nomodeset"
+# Replace
+# GRUB_CMDLINE_LINUX_DEFAULT="nouveau.blacklist=1 quiet splash nomodeset"
 
-#reboot
+# REMOVE ANY CONFLICTS
+# apt-get remove nvidia* && apt-get autoremove
 
-#ADD TO BLACKLIST
-#vim /etc/modprobe.d/blacklist.conf
-#blacklist vga16fb
-#blacklist nouveau
-#blacklist rivafb
-#blacklist nvidiafb
-#blacklist rivatv
-#blacklist lbm-nouveua
-#options nouveau modeset=0
-#alias nouveau off
-#alias lbm-nouveau off
+# reboot
 
-#INSTALL NVIDIA DRIVER
-#bash ./NVIDIA-Linux-x86_64*.run
+# ADD TO BLACKLIST
+# vim /etc/modprobe.d/blacklist.conf
+# blacklist vga16fb
+# blacklist nouveau
+# blacklist rivafb
+# blacklist nvidiafb
+# blacklist rivatv
+# blacklist lbm-nouveua
+# options nouveau modeset=0
+# alias nouveau off
+# alias lbm-nouveau off
 
-#nvidia-xconfig
+# INSTALL NVIDIA DRIVER
+# bash ./NVIDIA-Linux-x86_64*.run
 
-#reboot
+# nvidia-xconfig
 
-#vim /etc/default/grub
-#GRUB_CMDLINE_LINUX_DEFAULT="nouveau.blacklist=1 quiet splash nomodeset"
-#Replace
-#GRUB_CMDLINE_LINUX_DEFAULT="quiet splash nomodeset"
+# reboot
 
-#INSTALL STEAM AND ENJOY
+# vim /etc/default/grub
+# GRUB_CMDLINE_LINUX_DEFAULT="nouveau.blacklist=1 quiet splash nomodeset"
+# Replace
+# GRUB_CMDLINE_LINUX_DEFAULT="quiet splash nomodeset"
+
+# INSTALL STEAM AND ENJOY
